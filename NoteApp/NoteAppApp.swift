2d04118ebf571f140628a2b765b380cb65d98017
OliverAndreasen <oliv2058@stud.kea.dk>
2023-02-13 18:45:35 +0100

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Oliver Andreasen on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
